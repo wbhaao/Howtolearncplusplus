933a11250b9b5947985020ce77e734bbb12b0b6e
wbhaao <a31620504@gmail.com>
2024-02-03 19:59:31 +0900

    생성자 안만들면 버그나게 하는 코드

--- main.cpp
+++ main.cpp
@@ -1,42 +1,4 @@
-#include <iostream>
-
-class Date {
-    int year_;
-    int month_;  // 1 부터 12 까지.
-    int day_;    // 1 부터 31 까지.
-
+class Test {
 public:
-    void SetDate(int year, int month, int date);
-    void AddDay(int inc);
-    void AddMonth(int inc);
-    void AddYear(int inc);
-
-    // 해당 월의 총 일 수를 구한다.
-    int GetCurrentMonthTotalDays(int year, int month);
-
-    void ShowDate();
-
-    Date(int year, int month, int day) {
-        year_ = year;
-        month_ = month;
-        day_ = day;
-    }
+    Test() = default;  // 디폴트 생성자를 정의해라
 };
-
-// 생략
-
-void Date::AddYear(int inc) { year_ += inc; }
-
-void Date::ShowDate() {
-    std::cout << "오늘은 " << year_ << " 년 " << month_ << " 월 " << day_
-              << " 일 입니다 " << std::endl;
-}
-int main() {
-    Date day(2011, 3, 1);
-    day.ShowDate();
-
-    day.AddYear(10);
-    day.ShowDate();
-
-    return 0;
-}
