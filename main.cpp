a75f2c05da11b3b8242b7f1b7dca5f269b145fe2
wbhaao <a31620504@gmail.com>
2024-02-09 12:16:12 +0900

    배열

--- main.cpp
+++ main.cpp
@@ -1,35 +1,15 @@
 #include <iostream>
 
-int add(int a, int b) {
-    return a+b;
-}
-
-// 함수
 int main() {
-    // 이건 변수
-    int a = 1;
+    // 배열
+    int array[10] = {1, 2, 3, 4, 6, 7, 8, 9, 10};
+    array[1] = 3;
+    // 이걸 왜 에러 안띄우냐...
+    array[13] = 4;
+    printf("%d", *(array+13));
+    // 포인터
 
-    // 모듈화!!
-    // 기능 1 = 함수 1
 
-//     지역변수
-//     전역변수
-//     정적변수
-//     외부변수
-
-    // 함수
-    int data = add(10, 20);
     return 0;
 }
 
-/*
- 단축키
- 해당 부분 주석
- ctrl + /
- 종 방향도 선탱됨
- alt drag
- 실행
- Shift + F10
- 디버그
- Shift + F9
- */
